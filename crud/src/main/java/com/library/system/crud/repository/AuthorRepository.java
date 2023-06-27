2fd04c86ad0848ddf0b9c2e972301e23be78d173
Emily Koceja <emilysiquina@Emilys-Air.stiegleredtech.org>
2023-06-27 16:15:30 -0400

    starting from 0'

--- crud/src/main/java/com/library/system/crud/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.system.crud.repository;
-
-import com.library.system.crud.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- crud/src/main/java/com/library/system/crud/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.system.crud.repository;
-
-import com.library.system.crud.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
